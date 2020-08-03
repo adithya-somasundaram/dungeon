6c46c16ae12f641c203761bc98152e7ceb2ff4a2
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-08-03 12:22:13 -0700

    pseudocode

--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@ int main(){
     bool gameOver = false;
     int input;
 
+    // setup
     while(!gameOver){
         // render
         system("CLEAR");
+        // input
         cout << "Enter a number: ";
         cin >> input;
         if(input == 0){
             gameOver = true;
         }
+        // logic
     }
     return EXIT_SUCCESS;
 }
