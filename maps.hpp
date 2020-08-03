736d11aafd0239b88c7a841aa161714277d638ed
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-08-03 11:37:34 -0700

    test map

--- maps.hpp
+++ maps.hpp
@@ -1,11 +1,21 @@
 #ifndef MAPS
 #define MAPS
 
+#include <array>
+
 enum icons {
     W,      // wall
     P,      // path
     F,      // finish
     E       // enemy
+};
+
+int map_1[5][5] = {
+    {W, W, W, W, W},
+    {W, P, E, W, W},
+    {W, P, W, W, W},
+    {W, P, E, F, W},
+    {W, W, W, W, W},
 };
 
 #endif
