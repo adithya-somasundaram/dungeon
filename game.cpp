90feec7ddac753677f5115f187abb3f853966095
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-08-03 12:50:11 -0700

    added a few lines to game logic and draw

--- game.cpp
+++ game.cpp
@@ -1,9 +1,16 @@
 #include "game.hpp"
 
-void draw(Player *a, int map_numb){
+using namespace std;
 
+void draw(Player *a, int map_numb){
+    system("CLEAR");
 }
 
-bool logic(Player *a, int map_numb){
-    
+bool logic(Player *a, int map_numb, int move){
+
+    // end game if player is out of lives
+    if(a->getLives() > 0){
+        return true;
+    }
+    return false;
 }
