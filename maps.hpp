5a97a87a29c714eb3113412bde05b40063973e26
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-08-07 10:44:36 -0700

    update main map

--- maps.hpp
+++ maps.hpp
@@ -8,14 +8,17 @@ enum icons {
     E       // enemy
 };
 
-int map_1[5][5] = {
-    {W, W, W, W, W},
-    {W, P, E, W, W},
-    {W, P, W, W, W},
-    {W, P, E, F, W},
-    {W, W, W, W, W},
+int map_1[10][10] = {
+    {W, W, W, W, W, W, W, W, W, W},
+    {W, P, E, W, W, W, E, P, P, W},
+    {W, P, W, W, W, P, W, W, P, W},
+    {W, P, E, W, W, P, P, P, P, W},
+    {W, P, P, P, P, P, W, W, E, W},
+    {W, P, W, E, P, W, P, F, W, W},
+    {W, P, W, W, P, W, P, W, E, W},
+    {W, P, E, W, P, P, P, W, P, W},
+    {W, P, P, P, P, W, P, E, P, W},
+    {W, W, W, W, W, W, W, W, W, W}
 };
-
-// int maps [1][5][5] = {map_1};
 
 #endif
